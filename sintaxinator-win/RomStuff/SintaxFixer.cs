75ca331e03f7f75f8ea2b7e8708c0a337c61b5f3
taizou <cactusgunman@gmail.com>
2016-09-12 22:04:24 +0100

    remove a bunch of unused usings and require only .net framework 4.0. Failed attempt to improve compatibility with wine

--- sintaxinator-win/RomStuff/SintaxFixer.cs
+++ sintaxinator-win/RomStuff/SintaxFixer.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Security.Cryptography;
-using System.Collections;
 
 namespace RomStuff
 {
